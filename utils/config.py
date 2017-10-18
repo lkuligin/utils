6ed170a27df70bfd5ca6183812df517069de90cd
leo <lkuligin@yandex.ru>
2017-10-18 13:41:10 +0200

    stashing

---
+++ utils/config.py
@@ -0,0 +1,7 @@
+CLUSTER_NAME = 'DE-prod-jupiter-big'
+PROFILE_NAME = 'as24data AdminAccess'
+LOCAL_PORT_HIVE = 10001
+LOCAL_HOST_HIVE = 'localhost'
+KEY_PATH = '~/.aws/as24-bi.pem'
+SERVER_PORT_HIVE = 10000
+EC2_USER = 'hadoop'
